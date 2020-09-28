3845165f863d537a4db04e6cada3515ccf6e8a1a
Nina-Mathew <71400436+Nina-Mathew@users.noreply.github.com>
2020-09-28 13:40:48 -0400

    hungryness

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+// Created by: Nina Mathew
+// Created on: 9/28/20
+// To change a variable
+let hungryness = 0
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        hungryness = hungryness + 1
+        basic.showNumber(hungryness)
+    } else if (input.buttonIsPressed(Button.B)) {
+        hungryness = 0
+        basic.showNumber(hungryness)
+    }
 })
